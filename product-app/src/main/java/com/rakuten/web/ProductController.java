6d0eb6f45fa8e9c95600eed7a7cda06f53210c20
Ashish M J <ashm.jagadeesh@gmail.com>
2021-10-23 16:26:34 +0530

    Add rest api methods

--- product-app/src/main/java/com/rakuten/web/ProductController.java
+++ product-app/src/main/java/com/rakuten/web/ProductController.java
@@ -1,12 +1,19 @@
 package com.rakuten.web;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.rakuten.domain.Product;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.rakuten.service.ProductService;
 
@@ -21,4 +28,30 @@ public class ProductController {
 	public List<Product> getAllProducts(){
         return service.findAll();
 }
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable("id") int id) {
+		Product p = service.findById(id);
+		if(p != null) {
+			return new ResponseEntity<Product>(p, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+
+	@PostMapping("/products")
+	public ResponseEntity addProduct(@RequestBody Product toBeAdded) {
+		try {
+			int id = service.addProduct(toBeAdded);
+			HttpHeaders headers = new HttpHeaders();
+			headers.setLocation(URI.create("/products/"+id));
+			return new ResponseEntity(toBeAdded, headers, HttpStatus.CREATED);
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+  
+  
 }
+
